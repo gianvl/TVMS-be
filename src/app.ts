cfbc9854f16eb8bf629b4c6e00046de80a697316
Gian <91370424+gianvl@users.noreply.github.com>
2026-05-06 14:02:25 +0800

    Increase JSON body limit to 10mb

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cors({
   origin: ['http://localhost:5173', 'https://tvms-fe.vercel.app'],
   credentials: true,
 }));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 
 app.use('/api', routes);
